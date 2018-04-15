ceba6cfef5c1106f52f8619fdb48589d694a0f3c
zhouwangsheng <zhouwangsheng@gome.com.cn>
2018-04-15 18:30:10 +0800

    新增一个config模型

--- config.py
+++ config.py
@@ -2,6 +2,9 @@
 class Config(object):
 	pass
 
+class SomeConfig(object):
+	pass
+
 class DevConfig(Config):
 	DEBUG = True
 	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}'.format('root', '123456', 'localhost', '3306', 'mastering')
